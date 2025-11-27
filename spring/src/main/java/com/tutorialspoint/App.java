ee83daa88f2b52a9ce06fd83e27c214dbe6af458
Tony-Pereira05 <A13000882@alumnos.uady.mx>
2025-11-26 22:58:55 -0600

    Initial commit

---
+++ spring/src/main/java/com/tutorialspoint/App.java
@@ -0,0 +1,12 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloworld");
+        obj.getMessage();
+    }
+}
